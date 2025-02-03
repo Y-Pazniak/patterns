3228a0227bc0f9cb5bfaf22af59994bdbadf0ee3
Uladzimir Ilchanka <yauhenij@gmail.com>
2025-02-03 13:01:35 +0300

    Printf added

--- src/main/java/org/example/Bartender.java
+++ src/main/java/org/example/Bartender.java
@@ -3,6 +3,6 @@ package org.example;
 public class Bartender implements DrinkService {
     @Override
     public void serveDrink(final String name, final int age, final Drink drink) {
-        System.out.println("Made " + drink + " for " + name);
+        System.out.printf("Made %s for %s \n", drink, name);
     }
 }
